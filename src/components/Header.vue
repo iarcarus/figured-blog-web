08a0eb2a046643a9d16080be8b26da178f9d80a4
Medalha <andre.medalhaa@gmail.com>
2019-08-25 17:03:18 -0300

    cabecalho

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div style="display: flex; ">
+        <i class="large material-icons">person</i>
+        <p> {{getCorrectName()}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        methods: {
+            getCorrectName () {
+                console.log(this.$store.state.currentUser.user)
+                return this.$store.state.currentUser.user.name
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
